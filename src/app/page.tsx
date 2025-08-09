925d5920c821427cd1354a8f1d5f071b05e85857
josericardo03 <borges2001jose@gmail.com>
2025-08-09 15:03:08 -0400

    ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Team from "@/app/Components/Team";
+import Sobre from "@/app/Components/Sobre";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
-        <Team />
+        <Sobre />
       </div>
     </div>
   );
